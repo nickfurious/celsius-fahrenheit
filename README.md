800ed397ded53647676ee28d41be63e95a26a5a9
nickfurious <datfuriouskid@live.com>
2021-04-11 17:07:08 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# celsius-fahrenheit
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/celsius-fahrenheit)
---
+++ index.js
@@ -0,0 +1,11 @@
+// 1. Multiply Celsius value by 9/5
+// 2. Add 32 to Celsius value
+
+
+function convertToFahrenheit(celsius) {
+  let fahrenheit = celsius * 9/5 + 32
+
+  return fahrenheit;
+}
+
+console.log(convertToFahrenheit(30));
